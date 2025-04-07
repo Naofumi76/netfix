67f00c350603290ea1139608ee70f34a75954b56
Naofumi76 <sanniernathan@gmail.com>
2025-04-07 10:34:59 +0200

    Init commit

---
+++ users/forms.py
@@ -0,0 +1,121 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, authenticate
+from django.db import transaction
+from django.core.exceptions import ValidationError
+
+from .models import User, Company, Customer
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+def validate_email(value):
+    # In case the email already exists in an email input in a registration form, this function is fired
+    if User.objects.filter(email=value).exists():
+        raise ValidationError(
+            value + " is already taken.")
+
+
+class CustomerSignUpForm(UserCreationForm):
+    email = forms.EmailField(
+        validators=[validate_email],
+        widget=forms.TextInput(attrs={'placeholder': 'Enter Email'})
+    )
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Enter Username'})
+    )
+    birth = forms.DateField(
+        widget=DateInput(attrs={'placeholder': 'Date of Birth'}),
+        label='Birth Date'
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'birth')
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_customer = True
+        user.email = self.cleaned_data.get('email')
+        user.save()
+        
+        # Create the customer profile
+        customer = Customer.objects.create(
+            user=user,
+            birth=self.cleaned_data.get('birth')
+        )
+        
+        return user
+
+
+class CompanySignUpForm(UserCreationForm):
+    email = forms.EmailField(
+        validators=[validate_email],
+        widget=forms.TextInput(attrs={'placeholder': 'Enter Email'})
+    )
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Enter Username'})
+    )
+    field = forms.ChoiceField(
+        choices=(
+            ('Air Conditioner', 'Air Conditioner'),
+            ('All in One', 'All in One'),
+            ('Carpentry', 'Carpentry'),
+            ('Electricity', 'Electricity'),
+            ('Gardening', 'Gardening'),
+            ('Home Machines', 'Home Machines'),
+            ('House Keeping', 'House Keeping'),
+            ('Interior Design', 'Interior Design'),
+            ('Locks', 'Locks'),
+            ('Painting', 'Painting'),
+            ('Plumbing', 'Plumbing'),
+            ('Water Heaters', 'Water Heaters')
+        ),
+        widget=forms.Select(attrs={'placeholder': 'Field of Work'})
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'field')
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_company = True
+        user.email = self.cleaned_data.get('email')
+        user.save()
+        
+        # Create the company profile
+        company = Company.objects.create(
+            user=user,
+            field=self.cleaned_data.get('field')
+        )
+        
+        return user
+
+
+class UserLoginForm(forms.Form):
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs={'placeholder': 'Enter Email'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password'}))
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['autocomplete'] = 'off'
+        
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+        
+        if email and password:
+            try:
+                user = User.objects.get(email=email)
+                if not user.check_password(password):
+                    raise forms.ValidationError("Incorrect password")
+            except User.DoesNotExist:
+                raise forms.ValidationError("This email is not registered")
+        
+        return self.cleaned_data
